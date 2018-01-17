e3701931beb04010f84fb7ab3eb026bcbfdb1f6c
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-17 16:36:26 +0530

    ODDOREVEN.C
    Program to find whether the given number is odd or even.

---
+++ ODDOREVEN.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter a number");
+scanf("&n");
+if((n/2)=0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+}
