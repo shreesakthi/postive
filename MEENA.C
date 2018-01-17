10fcfd71a71943d9d2756a7f3eda9c07a73a3118
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-17 16:20:36 +0530

    MEENA.C
    program to find whether a given number is positive or not.

---
+++ MEENA.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("&n");
+if(n>0)
+{
+printf("number is positive");
+}
+else
+{
+printf("number is negative");
+}
+}
