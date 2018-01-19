56d1dd24ba48f4f3687bb7b9723b1430ecfdde78
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-19 10:33:50 +0530

    VOWEL.C
    program to find whether the given letter is vowel or consonant.

---
+++ VOWEL.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char x;
+printf("enter a letter");
+scanf("%c,&x");
+if(x=a,e,i,o,u)
+{
+printf("letter is vowel");
+}
+else
+{
+printf("letter is consonant");
+}
+}
