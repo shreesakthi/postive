cfb3095a012b7da6fc62da3ba6213c9cfcd27d80
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-21 16:30:54 +0530

    HELLOWORLD.C
    program to print helloworld n times.

---
+++ HELLOWORLD.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+clrscr();
+printf("enter the no. of times:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("HELLOWORLD");
+printf("\n");
+}
+getch();
+}
