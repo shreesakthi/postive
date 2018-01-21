61f5806647c87b6f1814c4fa29b3efc5672577ab
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-21 15:46:57 +0530

    ALPHABET.C
    program to find whether the character is alphabet or not.

---
+++ ALPHABET.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter a character:");
+scanf("%c",&a);
+if(c>=a && c<=b)
+{
+printf("it is alphabet");
+}
+else
+{
+printf("it is not a character");
+}
+}
