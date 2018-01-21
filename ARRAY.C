863519c2f023b673370bf990c4d5633c70b216fb
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-21 16:58:44 +0530

    Create ARRAY.C

---
+++ ARRAY.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,N,k,i;
+printf("enter the 2 numbers");
+scanf("%d %d",&N,&k);
+printf("enter the no. of elements");
+scanf("%d",&n);
+printf("enter the elements:);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&n[i]);
+}
