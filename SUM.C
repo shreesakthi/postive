de973005b80f6d452ec7527e73e49a929154f5dc
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-21 16:45:07 +0530

    SUM.C
    proggram to find sum of n numbers.

---
+++ SUM.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,n,sum=0,a;
+	printf("enter the number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		sum(i)=a;
+	}
+	printf("the sum is %d",a);
+	return 0;
+}
