6a99ee6857b6a644708eff89c2f92e9860f44fb9
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-22 16:37:00 +0530

    DIGIT.C
    program to count the given digit.

---
+++ DIGIT.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+int count=0;
+printf("\n enter an integer");
+scanf("%d",&n);
+while(n!=0)
+{
+n/=10;
+++count;
+}
+printf("number of digits :%d",count);
+return 0;
+}
