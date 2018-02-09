5bf71c31a2e2ec8412531cb90af9b0e848bc57e2
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-02-09 16:45:06 +0530

    INDEX.C
    program to print given numbers along with their index number

---
+++ INDEX.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) 
+{
+int a[10],i,j,n;
+printf("enter the no. of terms");
+scanf("%d",&n);
+printf("\nenter the numbers");
+for(i=0;i<=a[i];i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=0;j<=n;j++)
+{
+		printf("\n%d  %d",i,j);
+	
+	}
+	return 0;
+}
