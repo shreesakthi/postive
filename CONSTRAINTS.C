244f6676d49b28dd1c592409d5c8d01bddea25cf
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-02-09 15:49:49 +0530

    CONSTRAINTS.C
    program to sort the given array with improved constraints.

---
+++ CONSTRAINTS.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a[50],i,j,n,m;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=0;i<=n-1;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp;
+ }
+  }
+  }
+  if(n%2==0)
+  {
+ m=n/2;
+  printf("The sorted array is...%d",a[m]);
+}
+return 0;
+}
