8e9ad9269eb7f783bc2dbfe07117e58fd007bc17
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-02-09 15:53:27 +0530

    CONSTRAINTS.C
    program to sort the given numbers with improved constraints

--- CONSTRAINTS.C
+++ CONSTRAINTS.C
@@ -4,6 +4,9 @@ int main()
 int a[50],i,j,n,m;
 printf("Enter the number");
 scanf("%d",&n);
+ printf("enter the array elements");
+ for(i=0;i<=n;i++)
+  scanf("%d",&i);
 for(i=0;i<=n-1;i++)
 {
 for(j=i+1;j<=n;j++)
