bcf886bc7575da4f3cc88392cd4c33b387c10178
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-02-28 15:58:55 +0530

    ADD.C
    program to add two numbers

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	printf("enter a elements");
+	scanf("%d",&a);
+	printf("\nenter another element");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\nthe sum is %d",c);
+	return 0;
+}
