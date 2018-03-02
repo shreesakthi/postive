5904392828ac53087dcb0125f6fb6da479cec588
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-03-02 16:26:25 +0530

    ALPHANUM.C
    Program to check whether both alphabets and numbers are present in a string

---
+++ ALPHANUM.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+char str[50],a,z;
+printf("enter a string");
+scanf("%s",&str);
+if(str>a && str<z)
+{
+	printf("\nno");
+}
+else if(str>1 && str<9 && str>a && str<z)
+{
+	printf("\nyes");
+}
+	return 0;
+}
