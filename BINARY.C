9cfd15296d2e1b2f49d7638fedec7e880a5a021e
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-03-05 16:21:39 +0530

    BINARY.C
    program to check given number is binary or not

---
+++ BINARY.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int n[50],i;
+	printf("enter a number");
+	scanf("%d",&n[i]);
+	if(n[i]==0  && n[i]==1)
+	{
+		printf("binary number");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
