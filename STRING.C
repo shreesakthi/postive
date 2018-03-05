d74e311a56613e9044d8bf526b6ba330aec5caec
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-03-05 16:29:18 +0530

    STRING.C
    program to print number of strings

---
+++ STRING.C
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+void main()
+{
+    char s[50];
+    int count = 0, i;
+    printf("enter a few string\n");
+    scanf("%[^\n]s", s);
+    for (i = 0;s[i] !='0';i++)
+    {
+        if (s[i] == ' ')
+        {
+            count++;
+        }
+    }
+    printf("number of words in given string are: %d\n", count + 1);
+}
