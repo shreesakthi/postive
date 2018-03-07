db007af50872ac593d13f5c3db486474d9a8be22
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-03-07 15:51:20 +0530

    SEVENMULTIPLE.C
    program to check whether the given number is multiple of seven

---
+++ SEVENMULTIPLE.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,n;
+	printf("enter a number");
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
