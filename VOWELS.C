809ef3ea13924cd2e073e90efb8fff13eca3f2ee
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-03-09 15:47:24 +0530

    VOWELS.C
    program to check whether vowel is present in a string or not.

---
+++ VOWELS.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	char str,a,e,i,o,u,z;
+	int flag;
+	printf("enter a string");
+	scanf("%s",&str);
+	for(i=a;i<=z;i++)
+	{
+		if(str=='a' && str=='e' && str=='i' && str=='o' && str=='u')
+		{
+			flag=1;
+		}
+	}
+	if(flag==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
