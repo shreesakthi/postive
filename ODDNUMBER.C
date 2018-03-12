4c5dc145b7e9b964f9f531583391ab44799705a5
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-03-12 16:07:10 +0530

    ODDNUMBER.C
    program to print odd numbers in a given number

---
+++ ODDNUMBER.C
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+
+int main(){
+       int num,rem,digit;
+        printf("  Enter an integer number: ");
+        scanf("%d",&num);
+        while(num>0)
+        {
+               digit = num % 10;
+               num = num / 10;
+               rem = digit % 2;
+               if(rem != 0)
+               printf("\n  %d",digit);
+        }
+        return 0; 
+
+    } 
