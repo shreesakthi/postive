bbd6fac5240f265165f0385380273b449eab9cee
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-03-12 16:30:35 +0530

    AREA.C
    program to print area in float

---
+++ AREA.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	float a,b,c;
+	printf("enter the length");
+	scanf("%f",&a);
+	printf("enter the width");
+	scanf("%f",&b);
+	c=a*b;
+	printf("the area is %f",c);
+	return 0;
+}
